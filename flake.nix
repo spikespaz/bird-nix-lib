57822570494466150d7a413f12827d32c0138643
Jacob Birkett <spikespaz@outlook.com>
2023-09-18 01:01:41 -0700

    flake: lib: export overlay and lib separately

--- flake.nix
+++ flake.nix
@@ -10,11 +10,28 @@
 
   outputs = { self, nixpkgs-lib, systems, nixfmt, ... }:
     let
-      lib = nixpkgs-lib.lib.extend (self.lib);
+      inherit (self.lib) lib;
       eachSystem = lib.genAttrs (import systems);
     in {
-      lib = import ./lib;
+      lib = {
+        # ```nix
+        # let
+        #   lib = nixpkgs.lib.extend (inputs.bird-nix-lib.lib.overlay);
+        # in
+        # ```
+        overlay = import ./lib;
+        # ```nix
+        # let
+        #   lib = inputs.birdos.lib.lib;
+        # in
+        # ```
+        lib = nixpkgs-lib.lib.extend (self.lib.overlay);
+      };
+      # $ nix flake check
+      # or
+      # $ nix eval 'path:.#tests'
       tests = lib.birdos.runTestsRecursive ./tests { inherit lib; };
+      # $ nix fmt
       formatter = eachSystem (system: nixfmt.packages.${system}.default);
     };
 }
