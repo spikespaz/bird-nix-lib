1d54b8a75224578e9bcc538a8e24f22beea0564d
Jacob Birkett <spikespaz@outlook.com>
2023-09-18 15:12:11 -0700

    lib: builders: mkDirEntry: introduce exists

--- lib/builders.nix
+++ lib/builders.nix
@@ -144,6 +144,7 @@ let
     in rec {
       inherit name type;
       path = "${pathPrefix}/${baseName}";
+      exists = builtins.pathExists path;
       baseName = baseNameOf name;
       pathPrefix = toString dirName;
       relPath = if _hasPrefix then lib.removePrefix pathPrefix name else name;
@@ -157,11 +158,10 @@ let
       isProject = !isHidden && (isFile || isDir);
       isNixFile = isFile && lib.hasSuffix ".nix" baseName;
       isDefault = isFile && baseName == "default.nix";
-      hasDefault = let ls = builtins.readDir "${path}";
-      in isDir && ls ? "default.nix" && ls."default.nix" == "regular";
+      hasDefault = isDir && lib.pathIsRegularFile "${path}/default.nix";
       hasNixFiles =
         let ls = lib.mapAttrsToList (mkDirEntry path) (builtins.readDir path);
-        in isDir
+        in exists && isDir
         && (builtins.any (it: it.isNixFile || (it.isDir && it.hasNixFiles)) ls);
       isNix = isProject && (isNixFile || (isDir && hasNixFiles));
     };
