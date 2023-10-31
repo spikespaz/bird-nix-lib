9999cb461d2b20525e163777a2a87ddec75d6311
Jacob Birkett <spikespaz@outlook.com>
2023-10-30 18:49:22 -0700

    lib: attrsets: mapRecursiveCond: init new fn

--- lib/attrsets.nix
+++ lib/attrsets.nix
@@ -12,6 +12,23 @@ let
   # Take a list of attribute sets, recursively updating them into one.
   recursiveUpdates = builtins.foldl' lib.recursiveUpdate { };
 
+  # Same rules as `lib.mapAttrsRecursiveCond` but also recurses into lists.
+  #
+  # This implies:
+  # 1. The `path` passed to `op` as the first argument may contain indices.
+  # 2. The `cond` and `op` must check the type of
+  #    the expression it recieves as the second argument.
+  mapRecursiveCond = cond: op: expr:
+    let
+      recurse = path: expr:
+        if lib.isList expr && cond expr then
+          lib.imap0 (i: recurse (path ++ [ i ])) expr
+        else if lib.isAttrs expr && cond expr then
+          lib.mapAttrs (name: recurse (path ++ [ name ])) expr
+        else
+          op path expr;
+    in recurse [ ] expr;
+
   # TODO doc or remove
   thruAttr = attrName: attrs:
     if lib.isAttrs attrs && attrs ? ${attrName} then
@@ -97,6 +114,6 @@ let
     ];
 in {
   #
-  inherit updates hasAttrs hasExactAttrs recursiveUpdates thruAttr mapThruAttr
-    mapListToAttrs attrPaths importDir;
+  inherit updates hasAttrs hasExactAttrs recursiveUpdates mapRecursiveCond
+    thruAttr mapThruAttr mapListToAttrs attrPaths importDir;
 }
