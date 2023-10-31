9b6b89fb062387300b42e0829ae459873535d200
Jacob Birkett <spikespaz@outlook.com>
2023-10-31 00:52:20 -0700

    tests: lib: attrsets: add tests for hasExactAttrs

--- tests/lib/attrsets.nix
+++ tests/lib/attrsets.nix
@@ -22,4 +22,43 @@ lib.bird.mkTestSuite {
       expect = false;
     }
   ];
+  hasExactAttrs = [
+    {
+      name = "has exactly attr names";
+      expr = lib.hasExactAttrs [ "a" "b" "c" ] {
+        a = null;
+        b = null;
+        c = null;
+      };
+      expect = true;
+    }
+    {
+      name = "has too many attr names";
+      expr = lib.hasExactAttrs [ "a" "b" "c" ] {
+        a = null;
+        b = null;
+        c = null;
+        d = null;
+      };
+      expect = false;
+    }
+    {
+      name = "has too few attr names";
+      expr = lib.hasExactAttrs [ "a" "b" "c" ] {
+        a = null;
+        b = null;
+      };
+      expect = false;
+    }
+    {
+      name = "does not have all attr names";
+      expr = lib.hasExactAttrs [ "a" "b" "c" "e" ] {
+        a = null;
+        b = null;
+        c = null;
+        d = null;
+      };
+      expect = false;
+    }
+  ];
 }
